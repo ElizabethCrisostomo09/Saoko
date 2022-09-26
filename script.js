f7348464ae1ea18d5b70ee58c435b4baf5d313a5
Elizabeth <elizabethcrisostomo09@hotmail.com>
2022-09-26 10:52:50 -0500

    feat: uso de script externo

---
+++ script.js
@@ -0,0 +1,7 @@
+
+console.log("Saoko papi saoko");
+document.getElementById("generation").style.color = "grey";
+function colorTitulo(){
+  console.log("pulso el boton de Generation");
+  document.getElementById("generation").style.color = "#e7008a";
+}
